2c4953dbc3894c09fde880cd75cfd539d51b78ad
Ole Skaar <olejohska@gmail.com>
2017-03-22 12:15:59 +0100

    Work in progress
    Added regular expression for detecting domain, and the push to links array function. WIP: moving the code to functions, creating objects for each language. Eventually users will be able to enter their own domains in the front end.

--- linkcruncher.js
+++ linkcruncher.js
@@ -13,6 +13,8 @@ If value is match with lang1, add lang1domain in front of string
 For each link in links array, open each link
 */
 
+var langaugeProto
+
 var lang1 = ['studier-og-kurs', 'studere-ved-bi', 'alumni-og-partnerskap', 'forskning', 'om-bi'];
 var lang2 = ['programmes-and-individual-courses', 'study-at-bi', 'alumni-og-partnerskap', 'research', 'about-bi'];
 var lang1domain = 'https://www.bi.no';
@@ -20,33 +22,49 @@ var lang2domain = 'https://www.bi.edu';
 var inputfield = document.getElementById('inputfield');
 var links = [];
 var re = /\"(.*?)\"/g;
-
-var str = '<li><a href="/studier-og-kurs/bachelorstudier/business-administration/">Bachelor of Business Administration</a></li><li><a href="/studier-og-kurs/bachelorstudier/creative-industries-management/"> Creative Industries Management</a></li><li><a href="/studier-og-kurs/bachelorstudier/eiendomsmegling/"> Eiendomsmegling</a></li><li><a href="/studier-og-kurs/bachelorstudier/entreprenorskap-og-okonomi/"> Entreprenørskap og økonomi</a></li><li><a href="/studier-og-kurs/bachelorstudier/finans/"> Finans</a></li><li><a href="/studier-og-kurs/bachelorstudier/internasjonal-markedsforing/"> Internasjonal markedsføring</a></li><li><a href="/studier-og-kurs/bachelorstudier/markedsforingsledelse/"> Markedsføringsledelse</a></li><li><a href="/studier-og-kurs/bachelorstudier/pr-og-markedskommunikasjon/"> PR og markedskommunikasjon</a></li><li><a href="/studier-og-kurs/bachelorstudier/regnskap-og-revisjon/"> Regnskap og revisjon</a></li><li><a href="/studier-og-kurs/bachelorstudier/retail-management/"> Retail Management</a></li><li><a href="/studier-og-kurs/bachelorstudier/sivilokonom/">Siviløkonom</a></li><li><a href="/studier-og-kurs/bachelorstudier/okonomi-og-administrasjon/"> Økonomi og administrasjon</a></li><li><a href="/studier-og-kurs/bachelorstudier/okonomi-og-forretningsjus/"> Økonomi og forretningsjus</a></li>';
-
-var regExed;
-var result;
-var replaceQuote;
+var reLang = /\/(.*?)\//;
+var regExed, result;
 
 function replacer() {
+    // After 4ms, search input in input field for links, see if links belongs to language 1 or 2, add domain, and push results to links array
     setTimeout(function () {
         while ((regExed = re.exec(inputfield.value)) !== null) {
-            result = regExed[0];
+            result = regExed[1];
+                if (reLang.exec(result)[1] === lang1[0] || lang1[1] || lang1[2] || lang1[3])
+                {
+                    result = lang1domain + result;
+                } else if (reLang.exec(result)[1] === lang2[0]) {
+                    result = lang2domain + result;
+                } else { 
+                    alert(result +' does not belong to ' + lang1domain + ' or ' + lang2domain + '.');
+                }
             links.push(result);
-            }
-        for (i = 0; i < links.length; i++) {
-            console.log('hi');
-            replaceQuote = links[i].replace(/\"/g, '');
-            links[i] = replaceQuote;
-            }
-    }, 4);
 
+            }
+        }, 4);
 }
 
+/*
 
-/* 
-function openWindow() {
-    for (i = 0; i < 5; i++) {
+//Need to make an object for each language. 
+function langSearch(arr, l) {
+    for (i = 0; i < l.length; i++) {
+    if (reLang.exec(arr[1] === l[i]) {
+        result = lang1domain + result;
+    }
+}
+*/
+
+if ((regExed = re.exec(inputfield.value)) !== null) {
+    alert('No links found!');
+    }
+
+function openLinks() {
+    for (i = 0; i < links.length; i++) {
         window.open(links[i]);
     }
 }
+
+/* List for testing: 
+<li><a href="/studier-og-kurs/bachelorstudier/business-administration/">Bachelor of Business Administration</a></li><li><a href="/studier-og-kurs/bachelorstudier/creative-industries-management/"> Creative Industries Management</a></li><li><a href="/studier-og-kurs/bachelorstudier/eiendomsmegling/"> Eiendomsmegling</a></li><li><a href="/studier-og-kurs/bachelorstudier/entreprenorskap-og-okonomi/"> Entreprenørskap og økonomi</a></li><li><a href="/studier-og-kurs/bachelorstudier/finans/"> Finans</a></li><li><a href="/studier-og-kurs/bachelorstudier/internasjonal-markedsforing/"> Internasjonal markedsføring</a></li><li><a href="/studier-og-kurs/bachelorstudier/markedsforingsledelse/"> Markedsføringsledelse</a></li><li><a href="/studier-og-kurs/bachelorstudier/pr-og-markedskommunikasjon/"> PR og markedskommunikasjon</a></li><li><a href="/studier-og-kurs/bachelorstudier/regnskap-og-revisjon/"> Regnskap og revisjon</a></li><li><a href="/studier-og-kurs/bachelorstudier/retail-management/"> Retail Management</a></li><li><a href="/studier-og-kurs/bachelorstudier/sivilokonom/">Siviløkonom</a></li><li><a href="/studier-og-kurs/bachelorstudier/okonomi-og-administrasjon/"> Økonomi og administrasjon</a></li><li><a href="/studier-og-kurs/bachelorstudier/okonomi-og-forretningsjus/"> Økonomi og forretningsjus</a></li>
 */
