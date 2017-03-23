de2be50fac5e88a3fe14b8f7ab476e331ed1b67c
Ole Skaar <olejohska@gmail.com>
2017-03-23 22:30:48 +0100

    Version 0.2
    Changed the script to work with objects and functions instead of scripted values.
    Each domain is now an object, with directories and domains as properties
    A function has been added to add new languages
    
    To do: 
    - bug fix handling of bi.edu domains
    - add else statements and alerts for when there are no matches
    - create user interface, with the option to add regex parameters and new languages

--- linkcruncher.js
+++ linkcruncher.js
@@ -1,26 +1,28 @@
-//Link Cruncher 0.1
+//Link Cruncher 0.2
 /* A script that searches input text for URLs, checks which language the page is by scanning the first directory of the URI, returns the correct domain (default value bi.no/edu), then opens the links */
-
-
 /*
-Declare arrays lang1, lang2
-Declare var lang1domain and var lang2domain
-Create links array
+Declare domains array
+Create objects for each domain (default: bi.no and bi.edu)
 On paste, search input for values between two characters (default: "")
+For each value in array, check if first value between / / is equal to any value in any of the given languages
+Add the appropriate domain
 Place each value in links array
-For each value in array, if first character is /, check if value between / / is equal to any value in lang1 array, if not, check language2
-If value is match with lang1, add lang1domain in front of string
 For each link in links array, open each link
 */
 
-function Language(firstDirectory, domain) {
+var domains = [];
+
+function Domain(firstDirectory, domain) {
+    // Create a new domain and push domain to domains array
     this.firstDirectory = firstDirectory;
     this.domain = domain;
+    domains.push(this);
 }
 
-var bino = new Language(['studier-og-kurs', 'studere-ved-bi', 'alumni-og-partnerskap', 'forskning', 'om-bi'], 'https://www.bi.no');
 
-var biedu = new Language(['programmes-and-individual-courses', 'study-at-bi', 'alumni-og-partnerskap', 'research', 'about-bi'], 'https://www.bi.edu');
+var bino = new Domain(['studier-og-kurs', 'studere-ved-bi', 'alumni-og-partnerskap', 'forskning', 'om-bi'], 'https://www.bi.no');
+
+var biedu = new Domain(['programmes-and-individual-courses', 'study-at-bi', 'alumni-og-partnerskap', 'research', 'about-bi'], 'https://www.bi.edu');
 
 
 var inputfield = document.getElementById('inputfield');
@@ -30,41 +32,36 @@ var reLang = /\/(.*?)\//;
 var regExed, result;
 
 function replacer() {
-    // After 4ms, search input in input field for links, see if links belongs to language 1 or 2, add domain, and push results to links array
+    // After 4ms, search input field for links, and activate language search function
     setTimeout(function () {
         while ((regExed = re.exec(inputfield.value)) !== null) {
-            result = regExed[1];
-            
-                
+        langSearch(regExed[1], domains);
                 } 
-      
-             
-            links.push(result);
-
-            
         }, 4);
 }
 
-/*
-function langSearch(arr, l) {
-    for (i = 0; i < l.length; i++) {
-            if (reLang.exec(arr[1] === l[i])) {
-                result = l.domain + result;
-                }
+function langSearch(reg, d) {
+    // See if links belongs to any of the specified languages, if match, add domain, and push results to links array
+    for (var i = 0; i < d.length; i++) {
+        for (var j = 0; j < d[i].firstDirectory.length; j++) {
+            if (reLang.exec(reg)[1] === d[i].firstDirectory[j]) {
+            result = d[i].domain + reg;
+            links.push(result);
             }
+        }
+    }
+}
 
+function openLinks() {
+    for (i = 0; i < links.length; i++) {
+        window.open(links[i]);
+    }
+}
+
+/*
 // else statement for when language does not exist
 else { 
                     alert(result +' does not belong to ' + lang1domain + ' or ' + lang2domain + '.');
-                }
-
-//If result of regex matches with first step of lang 1, add lang1domain to result, else same with lang2
-           
-           if (reLang.exec(result)[1] === lang1[0] || lang1[1] || lang1[2] || lang1[3])
-                {
-                    result = lang1domain + result;
-                } else if (reLang.exec(result)[1] === lang2[0]) {
-                    result = lang2domain + result;
                 }
                 
 */
@@ -73,11 +70,7 @@ if ((regExed = re.exec(inputfield.value)) !== null) {
     alert('No links found!');
     }
 
-function openLinks() {
-    for (i = 0; i < links.length; i++) {
-        window.open(links[i]);
-    }
-}
+
 
 /* List for testing: 
 <li><a href="/studier-og-kurs/bachelorstudier/business-administration/">Bachelor of Business Administration</a></li><li><a href="/studier-og-kurs/bachelorstudier/creative-industries-management/"> Creative Industries Management</a></li><li><a href="/studier-og-kurs/bachelorstudier/eiendomsmegling/"> Eiendomsmegling</a></li><li><a href="/studier-og-kurs/bachelorstudier/entreprenorskap-og-okonomi/"> Entreprenørskap og økonomi</a></li><li><a href="/studier-og-kurs/bachelorstudier/finans/"> Finans</a></li><li><a href="/studier-og-kurs/bachelorstudier/internasjonal-markedsforing/"> Internasjonal markedsføring</a></li><li><a href="/studier-og-kurs/bachelorstudier/markedsforingsledelse/"> Markedsføringsledelse</a></li><li><a href="/studier-og-kurs/bachelorstudier/pr-og-markedskommunikasjon/"> PR og markedskommunikasjon</a></li><li><a href="/studier-og-kurs/bachelorstudier/regnskap-og-revisjon/"> Regnskap og revisjon</a></li><li><a href="/studier-og-kurs/bachelorstudier/retail-management/"> Retail Management</a></li><li><a href="/studier-og-kurs/bachelorstudier/sivilokonom/">Siviløkonom</a></li><li><a href="/studier-og-kurs/bachelorstudier/okonomi-og-administrasjon/"> Økonomi og administrasjon</a></li><li><a href="/studier-og-kurs/bachelorstudier/okonomi-og-forretningsjus/"> Økonomi og forretningsjus</a></li>
